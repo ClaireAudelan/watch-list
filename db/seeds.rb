cc8d9a106676be0d8cf4be0cc8915225336c7012
Claire Lo <caudelan@gmail.com>
2022-01-22 14:49:22 +0000

    finish seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require "open-uri"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+puts movies["results"]
+
+puts "Cleaning up database"
+Movie.destroy_all
+puts "Database is clean"
+
+puts "Creating movies"
+movies["results"].each do |result|
+  movie = Movie.create(
+    title: result["title"],
+    overview: result["overview"],
+    poster_url: result["poster_path"],
+    rating: result["vote_average"]
+  )
+  puts "Movie #{movie.id} is created"
+end
+
+puts "Done!"
